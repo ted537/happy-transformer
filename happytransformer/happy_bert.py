9c01fb5f9c885b79373b28baf3f64a4975649bd0
uGokalp <15ug1@queensu.ca>
2020-01-14 14:16:35 -0500

    Fixed indentation of load_finetuned

--- happytransformer/happy_bert.py
+++ happytransformer/happy_bert.py
@@ -108,15 +108,14 @@ class HappyBERT(HappyTransformer):
                     break
         return True
 
+    def load_finetuned(self, model, tokenizer):
+        """
+        Allows the user to use their finetuned model on masked word prediction
 
-def load_finetuned(self, model, tokenizer):
-    """
-    Allows the user to use their finetuned model on masked word prediction
-
-    :param model: Model created by FinetuneMlm.train
-    :param tokenizer: Tokenizer created by FinetuneMlm.train
-    """
-    self.mlm = model
-    self.mlm.eval()
-    self.tokenizer = tokenizer
-    print("You may use your finetuned model for masked word prediction")
+        :param model: Model created by FinetuneMlm.train
+        :param tokenizer: Tokenizer created by FinetuneMlm.train
+        """
+        self.mlm = model
+        self.mlm.eval()
+        self.tokenizer = tokenizer
+        print("You may use your finetuned model for masked word prediction")
